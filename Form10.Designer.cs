57d873f309855aec7953457d9d86406b898f87aa
root <mgiraudo@ies21.edu.ar>
2023-09-29 12:56:16 -0300

    ...

--- Form10.Designer.cs
+++ Form10.Designer.cs
@@ -72,7 +72,7 @@ namespace Remis_Ya
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(189, 17);
+            this.button1.Location = new System.Drawing.Point(187, 49);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
