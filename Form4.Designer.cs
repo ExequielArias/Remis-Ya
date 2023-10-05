9f544d0d5e4326ed70940763a3da482edfd59a2e
Exequiel <Exequiel@DESKTOP-4JNCILO>
2023-10-05 13:36:52 -0300

    5/10/23

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -30,15 +30,16 @@ namespace Remis_Ya
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtpFechaHasta = new System.Windows.Forms.DateTimePicker();
+            this.dgvForm4 = new System.Windows.Forms.DataGridView();
             this.Viaje = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NombreChofer = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Importe = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.cmdBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvForm4)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -50,46 +51,46 @@ namespace Remis_Ya
             this.label1.TabIndex = 0;
             this.label1.Text = "DESDE";
             // 
-            // dateTimePicker1
+            // dtpFechaDesde
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(94, 19);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(112, 20);
-            this.dateTimePicker1.TabIndex = 1;
+            this.dtpFechaDesde.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaDesde.Location = new System.Drawing.Point(94, 19);
+            this.dtpFechaDesde.Name = "dtpFechaDesde";
+            this.dtpFechaDesde.Size = new System.Drawing.Size(112, 20);
+            this.dtpFechaDesde.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(336, 24);
+            this.label2.Location = new System.Drawing.Point(256, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "HASTA";
             // 
-            // dateTimePicker2
+            // dtpFechaHasta
             // 
-            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(385, 18);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(100, 20);
-            this.dateTimePicker2.TabIndex = 3;
+            this.dtpFechaHasta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaHasta.Location = new System.Drawing.Point(305, 18);
+            this.dtpFechaHasta.Name = "dtpFechaHasta";
+            this.dtpFechaHasta.Size = new System.Drawing.Size(100, 20);
+            this.dtpFechaHasta.TabIndex = 3;
             // 
-            // dataGridView1
+            // dgvForm4
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvForm4.AllowUserToAddRows = false;
+            this.dgvForm4.AllowUserToDeleteRows = false;
+            this.dgvForm4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvForm4.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Viaje,
             this.Fecha,
             this.NombreChofer,
             this.Importe});
-            this.dataGridView1.Location = new System.Drawing.Point(35, 64);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(594, 351);
-            this.dataGridView1.TabIndex = 4;
+            this.dgvForm4.Location = new System.Drawing.Point(35, 64);
+            this.dgvForm4.Name = "dgvForm4";
+            this.dgvForm4.ReadOnly = true;
+            this.dgvForm4.Size = new System.Drawing.Size(594, 351);
+            this.dgvForm4.TabIndex = 4;
             // 
             // Viaje
             // 
@@ -116,19 +117,30 @@ namespace Remis_Ya
             this.Importe.Name = "Importe";
             this.Importe.ReadOnly = true;
             // 
+            // cmdBuscar
+            // 
+            this.cmdBuscar.Location = new System.Drawing.Point(437, 18);
+            this.cmdBuscar.Name = "cmdBuscar";
+            this.cmdBuscar.Size = new System.Drawing.Size(75, 23);
+            this.cmdBuscar.TabIndex = 5;
+            this.cmdBuscar.Text = "BUSCAR";
+            this.cmdBuscar.UseVisualStyleBackColor = true;
+            this.cmdBuscar.Click += new System.EventHandler(this.cmdBuscar_Click);
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(661, 460);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.cmdBuscar);
+            this.Controls.Add(this.dgvForm4);
+            this.Controls.Add(this.dtpFechaHasta);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dtpFechaDesde);
             this.Controls.Add(this.label1);
             this.Name = "Form4";
             this.Text = "Form4";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvForm4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,13 +149,14 @@ namespace Remis_Ya
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpFechaDesde;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dtpFechaHasta;
+        private System.Windows.Forms.DataGridView dgvForm4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Viaje;
         private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
         private System.Windows.Forms.DataGridViewTextBoxColumn NombreChofer;
         private System.Windows.Forms.DataGridViewTextBoxColumn Importe;
+        private System.Windows.Forms.Button cmdBuscar;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -16,5 +16,26 @@ namespace Remis_Ya
         {
             InitializeComponent();
         }
+
+        private void cmdBuscar_Click(object sender, EventArgs e)
+        {
+            Choferes chofer = new Choferes();
+            Viajes viaje = new Viajes();
+            DataTable tablaChoferes = chofer.getData();
+            DataTable tablaViajes = viaje.getData();
+            DateTime desde = Convert.ToDateTime(dtpFechaDesde.Text);
+            DateTime hasta = Convert.ToDateTime(dtpFechaHasta.Text);
+            dgvForm4.Rows.Clear();
+            foreach (DataRow filaViaje in tablaViajes.Rows)
+            {
+                DateTime fechaViaje = Convert.ToDateTime(filaViaje["fecha"]);
+                if (fechaViaje >= desde && fechaViaje <= hasta)
+                {
+                    DataRow filaChofer = tablaChoferes.Rows.Find(Convert.ToInt32(filaViaje["chofer"]));
+                    string fecha = fechaViaje.ToString("dd/MM/yyyy");
+                    dgvForm4.Rows.Add(filaViaje["viaje"], fecha, filaChofer["nombre"], filaViaje["importe"]);
+                }
+            }
+        }
     }
 }
